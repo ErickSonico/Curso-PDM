e07180831f7f1a10e0cee4e96e61f40ae24a4ca4
ericksonico <erick_sonico@ciencias.unam.mx>
2026-04-20 18:51:54 -0600

    Tarea 3

--- Tareas/Tarea02/app/src/main/java/erick/pdm/tarea02/MainActivity.java
+++ Tareas/Tarea02/app/src/main/java/erick/pdm/tarea02/MainActivity.java
@@ -1,25 +1,56 @@
 package erick.pdm.tarea02;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Tarea02_Log";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.personajes);
-        // setContentView(R.layout.biografia);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        // Abrir menú lateral
+        toolbar.setNavigationOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
+
+        // Menú de tres puntitos
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.action_search) {
+                Log.d(TAG, "Buscar");
+                return true;
+            } else if (item.getItemId() == R.id.action_info) {
+                Log.d(TAG, "Información de la app");
+                return true;
+            } else if (item.getItemId() == R.id.acerca_de) {
+                Log.d(TAG, "Viva Halo");
+                return true;
+            }
+            return false;
+        });
+
+        // Opciones de personajes
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_add) {
+                Log.d(TAG, "Agregar personaje");
+            } else if (id == R.id.nav_delete) {
+                Log.d(TAG, "Eliminar personaje");
+            } else if (id == R.id.nav_edit) {
+                Log.d(TAG, "Editar personaje");
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
         });
     }
 }
